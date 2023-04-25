fdb2ad18eb47052c67d0925b6e99545caaec653f
mdennisj <mdennisj@students.kennesaw.edu>
2023-04-25 16:23:32 -0400

    Added second dropdown to show states for north america

--- Class Code.py
+++ Class Code.py
@@ -1,44 +1,55 @@
 from tkinter import *
 from tkinter import ttk
-root = Tk()
-root.geometry("400x400")
 
-label1 = Label(root, text= "Global Travel Generator!")
-label1.pack()
 
-class Generator:
- 
-    def __init__ (self,master):
-        self.master = master
-        self.dropdown()
+
+class Travel_Explorer(Tk):
+    '''
+    EXPLAIN WHAT THIS OBJECT IS
+    '''
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
         
+        self.label1 = Label(self, text= "Global Travel Option Explorer!")
+        self.label1.pack()
+        self.states = ["Alabamba", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Deleware", "Flordia", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts","Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virgina", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
+        self.sacountries = ["Columbia", "Ecuador", "Bolivia", "Brazil", "Argentina", "Venezuela", "Chile", "Paraguay", "Peru", "Uruguay", "Suriname", "Guyana"]
+        self.continents=["Africa","Antartica","Asia", "Australia","Europe","North America", "South America"]
+        self.choose_continent()
+        self.geometry("400x400")
 
-    def dropdown(self):
+    def choose_continent(self):
         self.label1 = Label(self.master, text="Pick A Continent: ")
         self.label1.pack()
 
-        self.label_var = StringVar()
-        self.label_var.set("Click for Continent")
-        self.label_optionmenu = OptionMenu(self.master, self.label_var, "Africa","Antartica","Asia", "Australia","Europe","North America", "South America", command=self.continents)
-        self.label_optionmenu.pack()
+        self.selected_continent = StringVar()
+        self.selected_continent.set("Click for Continent")
+        self.optionmenu = OptionMenu(
+            self.master, 
+            self.selected_continent, 
+            *self.continents,
+            command=self.choose_country
+            )
+        self.optionmenu.pack()
 
         self.word_label = Label(self.master, text="")
         self.word_label.pack()
 
-    def continents(self, *args):
-        if self.dropdown == "South America":
+    def choose_country(self, *args):
+        if self.selected_continent.get() == "South America":
             label2 = Label(self.master, text="Pick a number (1-12):", command=self.south_america)
             label2.pack()
-        elif self.dropdown == "Asia":
+        elif self.selected_continent.get() == "Asia":
             label3 = Label(self.master, text="Pick a number(1-31): ")
             label3.pack()
-        elif self.dropdown == "North America":
+        elif self.selected_continent.get() == "North America":
             label4 = Label(self.master, text="Pick a numner (1-50):")
             label4.pack()
+            self.north_america()
     
 
     def south_america(self, *args):
-        number = int(self.dropdown.get())
+        number = int(self.choose_country.get())
         if number == 1:
             country = "Columbia"
         elif number == 2:
@@ -64,7 +75,24 @@ class Generator:
         elif number == 12:
             country= "Guyana"
         self.word_label.config(text=self.word_label)
+    
+    def north_america(self, *args):
+        #number2 = int(self.choose_country.get())-1
+        self.selected_state = StringVar()
+        self.selected_state.set("Click for State")
+        self.state_options = OptionMenu(
+            self.master, 
+            self.selected_state, 
+            *range(len(self.states))
+            #command=self.show_state
+        )
 
 
 
-root.mainloop()
+    
+
+        
+
+
+app1 = Travel_Explorer()
+app1.mainloop()
